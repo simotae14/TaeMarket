328b4a7cfbbf1b99397e125e646231d10f33c4e5
Simone Taeggi <startae14@gmail.com>
2016-10-20 09:36:45 +0000

    heroku paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,8 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV.fetch('AWS_REGION'),
     },
-    :s3_host_name => "s3-eu-west-1.amazonaws.com", # Added entry
-    :url => ":s3_host_name"                        # Added entry
+    :s3_host_name => "s3-us-west-2.amazonaws.com", # Added entry
+    :url => ":s3_host_name",                        # Added entry
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 end
