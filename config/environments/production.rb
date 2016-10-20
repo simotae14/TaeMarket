ba902f822d22ae929755e4f139ebc2065636d2b0
Simone Taeggi <startae14@gmail.com>
2016-10-20 09:30:30 +0000

    heroku paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-    url => ":s3_host_name"
+    :s3_host_name => "s3-eu-west-1.amazonaws.com", # Added entry
+    :url => ":s3_host_name"                        # Added entry
   }
 end
